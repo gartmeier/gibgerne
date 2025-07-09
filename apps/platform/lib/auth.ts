f2122cb4ade0de9a3d87764e3624356322ca1faa
Joshua Gartmeier <joshua@gartmeier.dev>
2025-07-09 10:52:56 +0200

    Add Better Auth admin plugin

--- apps/platform/lib/auth.ts
+++ apps/platform/lib/auth.ts
@@ -1,12 +1,11 @@
 import { db } from "@/db/drizzle";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
-
-import { organization } from "better-auth/plugins";
+import { admin, organization } from "better-auth/plugins";
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
-  plugins: [organization()],
+  plugins: [admin(), organization()],
 });
